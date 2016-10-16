bb758b258990a6fbfbe029904bb6f57ca527e961
nnthhg <nnthhg@icl.com>
2016-10-16 14:35:19 +0700

    Strategy pattern

--- DesignPatterns/StrategyPattern/Program.cs
+++ DesignPatterns/StrategyPattern/Program.cs
@@ -3,9 +3,61 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using static System.Console;
 namespace StrategyPattern
 {
+    public interface FlyBehavior
+    {
+        void PerformFly();
+    }
+    public class FlyWithWings : FlyBehavior
+    {
+        public void PerformFly()
+        {
+            WriteLine("Fly with wings!");
+        }
+
+    }
+    public interface QuackBehavior
+    {
+        void PerformQuack();
+    }
+    public class Quack : QuackBehavior
+    {
+        public void PerformQuack()
+        {
+            Console.WriteLine("Quack!");
+        }
+    }
+    public class Duck
+    {
+        protected FlyBehavior flyBehavior;
+        protected QuackBehavior quackBehavior;
+        public void PerformQuack()
+        {
+            quackBehavior.PerformQuack();
+        }
+        public void PerformFly()
+        {
+            flyBehavior.PerformFly();
+        }
+        public void Display()
+        {
+            WriteLine("I'm duck!");
+        }
+    }
+    public class MallardDuck : Duck
+    {
+        public MallardDuck()
+        {
+            flyBehavior = new FlyWithWings();
+            quackBehavior = new Quack();
+        }
+        public new void Display()
+        {
+            WriteLine("I'm Mallard duck!");
+        }
+    }
     class Program
     {
         //Design principle
@@ -18,6 +70,10 @@ namespace StrategyPattern
         //implementation.
         static void Main(string[] args)
         {
+            MallardDuck mallardDuck = new MallardDuck();
+            mallardDuck.PerformFly();
+            mallardDuck.PerformQuack();
+            mallardDuck.Display();
         }
     }
 }
