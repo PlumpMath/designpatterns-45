94073a96044001e87ec914863015c391490d77c1
nnthhg <nnthhg@icl.com>
2016-10-16 11:07:28 +0700

    Initial Commit

---
+++ DesignPatterns/StrategyPattern/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StrategyPattern
+{
+    class Program
+    {
+        //Design principle
+        //Identify the aspects of your
+        //application that vary and separate
+        //them from what stays the same.
+        
+        //Design principle
+        //Program to an interface (abstract class/interface), not an
+        //implementation.
+        static void Main(string[] args)
+        {
+        }
+    }
+}
